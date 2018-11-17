62c331d7acf54feaba9070399bdc27c8b2e88f82
ZigBot <zb@zigbot.us.to>
2018-11-17 15:11:29 -0600

    Added utility functions and folder

---
+++ utils/listservers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Author: TehZig#1949
+import discord
+import os
+
+curDir = os.path.dirname(os.path.realpath(__file__))
+config = open(curDir + "/../include/config")
+lines = config.readlines()
+TOKEN = lines[0].rstrip()
+zigID = lines[1].rstrip()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------\nPress [Ctrl+C] to view servers\n')
+
+client.run(TOKEN)
+
+servers = list(client.servers)
+print("\nConnected on " + str(len(client.servers)) + " servers:")
+for x in range(len(servers)):
+    print('  ' + servers[x-1].name + '\n    id: ' + servers[x-1].id)
