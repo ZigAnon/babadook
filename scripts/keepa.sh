e0d573936cb5af0692a4ca11224ac88a9101ef7a
ZigAnon <45124318+ZigAnon@users.noreply.github.com>
2018-11-17 08:24:59 -0600

    Set script to be system independent

--- scripts/keepa.sh
+++ scripts/keepa.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env /bin/bash
 
-PID=`cat /{some_folder}/your_app.pid`
+# Replace with your version
+pythonVersion='python3.6'
+
+PWD=$(pwd)
+PY3=$(which $pythonVersion)
+PID=`cat $PWD/../logs/main.pid`
 
 if ! ps -p $PID > /dev/null
 then
-	rm /{some_folder}/your_app.pid
-	{your_python_command} & echo $! >>/{some_folder}/your_app.pid
+	rm $PWD/../logs/main.pid
+	$PY3 $PWD/../main.py & echo $! >>$PWD/../logs/main.pid
 fi
