0d2d1d6357293993522c9d3a424551e732c6ec55
ZigBot <zb@us.to>
2018-11-27 20:05:40 -0600

    Rewrote client script to have bot class permissions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3.6
 # Author: TehZig#1949
 import discord
+from discord.ext import commands
 import asyncio
 import os
 
@@ -19,26 +20,32 @@ zigID = lines[1].rstrip()
 # Code to be added
 ############################
 
-client = discord.Client()
+zdesc = '''Thanks for using ZigBot!'''
 
-@client.event
+bot = commands.Bot(command_prefix='zb!', description=zdesc)
+
+@bot.event
 async def on_message(message):
     if message.content.startswith('.iam') and (discord.utils.get(message.author.roles, id = addRole) is None):
         rmrole = discord.utils.get(message.server.roles, id = rmRole)
         addrole = discord.utils.get(message.server.roles, id = addRole)
-        await client.add_roles(message.author, addrole)
+        await bot.add_roles(message.author, addrole)
         await asyncio.sleep(1)
-        await client.remove_roles(message.author, rmrole)
+        await bot.remove_roles(message.author, rmrole)
+    else:
+        # Needed per FAQ
+        # https://discordpy.readthedocs.io/en/latest/faq.html#why-does-on-message-make-my-commands-stop-working
+        await bot.process_commands(message)
 
 ############################
 # Code to be added
 ############################
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-client.run(TOKEN)
+bot.run(TOKEN)
