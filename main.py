7e8e26f72076107d09dd806c7444aa9e488160ef
ZigAnon <zb@zigbot.us.to>
2019-01-26 21:59:51 -0600

    Set roles to be global to aid in setting

--- main.py
+++ main.py
@@ -772,32 +772,43 @@ async def cleanpost(ctx, member: discord.Member):
 async def on_voice_state_update(before,after):
     with open(curDir + '/include/voice') as v:
         voiceID = [line.strip('\n').split(',') for line in v]
+    for x in range(len(voiceID)):
+        if int(after.voice.voice_channel.id) == int(voiceID[x-1][0]):
+            add = discord.utils.get(after.server.roles, id = voiceID[x-1][1])
+
+        if int(before.voice.voice_channel.id) == int(voiceID[x-1][0]):
+            rmv = discord.utils.get(before.server.roles, id = voiceID[x-1][1])
+
     if after.voice.voice_channel is not None:
-        for x in range(len(voiceID)):
-            if int(after.voice.voice_channel.id) == int(voiceID[x-1][0]):
-                see = discord.utils.get(after.server.roles, id = voiceID[x-1][1])
-                await bot.add_roles(after, see)
-                while True:
-                    if see in after.roles:
-                        break
-                    else:
-                        await bot.add_roles(after, see)
-                await asyncio.sleep(1)
+        await bot.add_roles(after, add)
+        while True:
+            if add in after.roles:
+                await bot.remove_roles(after, rmv)
                 break
-    if before.voice.voice_channel is not None:
-        for x in range(len(voiceID)):
-            if int(before.voice.voice_channel.id) == int(voiceID[x-1][0]):
-                hide = discord.utils.get(after.server.roles, id = voiceID[x-1][1])
-                await asyncio.sleep(5)
-                if int(before.voice.voice_channel.id) == int(voiceID[x-1][0]):
-                    await bot.remove_roles(after, hide)
-                    break
-    elif before.voice.voice_channel is None and after.voice.voice_channel is not None:
-        for x in range(len(voiceID)):
-            if int(after.voice.voice_channel.id) == int(voiceID[x-1][0]):
-                see = discord.utils.get(after.server.roles, id = voiceID[x-1][1])
-                await bot.add_roles(after, see)
+            else:
+                await bot.add_roles(after, add)
+    elif after.voice.voice_channel is None:
+        await bot.remove_roles(after, rmv)
+        while True:
+            if rmv in after.roles:
+                await bot.remove_roles(after, rmv)
+            else:
                 break
+    return
+#     if before.voice.voice_channel is not None:
+#         for x in range(len(voiceID)):
+#             if int(before.voice.voice_channel.id) == int(voiceID[x-1][0]):
+#                 hide = discord.utils.get(after.server.roles, id = voiceID[x-1][1])
+#                 await asyncio.sleep(5)
+#                 if int(before.voice.voice_channel.id) == int(voiceID[x-1][0]):
+#                     await bot.remove_roles(after, hide)
+#                     break
+#     elif before.voice.voice_channel is None and after.voice.voice_channel is not None:
+#         for x in range(len(voiceID)):
+#             if int(after.voice.voice_channel.id) == int(voiceID[x-1][0]):
+#                 add = discord.utils.get(after.server.roles, id = voiceID[x-1][1])
+#                 await bot.add_roles(after, add)
+#                 break
 
 ############################
 ############################
